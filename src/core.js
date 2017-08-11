038f9513a9fa92d0c322727ff4c5375a3e9438e6
Aaron Meredith <aaron@Aarons-iMac.local>
2017-08-11 17:03:54 +1200

    first

---
+++ src/core.js
@@ -0,0 +1,102 @@
+import is from 'is_js';
+
+class Parzen {
+
+    {{$a:a.b.c|an|h}}
+
+
+    constructor(props) {
+        this.REGEX_TAG = 
+        this.data = props.data;
+        this.variables = {};
+    }
+
+    getRandomWord(list)
+    {
+        return list[Math.floor(Math.random() * list.length)];
+    }
+
+    make(props)
+    {
+        var start = this.getListString(props.base);
+
+        var complete = this.recurse(start.str);
+
+    }
+
+    recurse(string)
+    {
+        if(is.not.string(string)){
+            string.replace(this.REGEX_TAG, this.parseTag);
+        }
+    }
+
+    parseTag()
+    {
+
+    }
+
+    getListString(name) 
+    {
+        //get object keys as name might be delcared like people.cloths.shoes
+        let path = name.split(".");
+        if(!path.length){
+            return null;
+        }
+
+        let result = {
+            index : null,
+            path : [],
+            str : ""
+        }
+
+        let data = this.data;
+
+        //handle nested lists
+        var current = "";
+        while(is.object(data) && is.not.array(data))
+        {             
+            if(path.length)
+            {
+                current = path.shift();    
+            }
+
+            //allow selecting which specific array look for arrays keywords with [0],[23] at the end.
+            var index = current.match(/([^\[\]]*)\[(\d+)\]/);      
+            if(index)
+            {
+                current = index[1];
+                if(!isNaN(index[2]))
+                {
+                    result.index = +index[2];
+                }
+            } 
+                   
+            //if path has ended or path node has failed get a random key from the next nested list
+            if( current == "" || !data.hasOwnProperty(current))
+            {
+                current = this.getRandomWord(Object.keys(data));
+            }
+            
+            result.path.push(current);
+            data = data[current];
+        }
+
+        //now actuallt
+        if(is.array(data))
+        {   
+            if(result.index != null)
+            {
+                result.str = data[result.index];
+            }
+            
+            if(result.index + 1 > data.lengh)
+            {
+                result.str = this.getRandomWord(data)
+            }
+        }    
+        return result;
+    }
+}
+
+window.Parzen = Parzen;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+ var path = require('path');
+ var webpack = require('webpack');
+     
+ module.exports = {
+    entry: './src/core.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'parzen.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/,
+                query: {
+                    presets: ['es2015', 'es2016', 'react']
+                }
+            }
+        ]
+    },
+    plugins: [
+        // new webpack.DefinePlugin({
+        //     'process.env': {
+        //         NODE_ENV: JSON.stringify('production')
+        //     }
+        // }),
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: {
+        //         warnings: false
+        //     }
+        // })
+    ],
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+ };
